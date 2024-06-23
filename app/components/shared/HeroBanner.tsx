c26c6f4614066d61a4f47e1aaebab2a60e4e7d52
Parker Freestone <parkerfreestone@gmail.com>
2024-06-23 14:52:34 -0600

    :sparkles: Minor fixes on nav, and hero banner

--- app/components/shared/HeroBanner.tsx
+++ app/components/shared/HeroBanner.tsx
@@ -1,15 +1,22 @@
 import { ArrowRight, MoveRight } from "lucide-react";
 import Banner from "../../../public/images/ai-cleaning-banner.webp";
+import { motion } from "framer-motion";
 
 //bg-hero-banner bg-no-repeat
 
 export const HeroBanner = () => {
   return (
-    <div className="mx-2 lg:mx-6 h-[200vh] transition-all">
-      <div className="w-full h-[60vh] bg-gray-900 bg-cover rounded-3xl px-[1rem] sm:px-[5rem] pb-[1rem] flex flex-col justify-center gap-6">
-        <h1 className="text-white text-6xl sm:text-7xl font-bold mt-auto sm:mt-0">
-          Your Expert <br /> Cleaning Services
-        </h1>
+    <div className="mx-2 lg:mx-6 transition-all">
+      <div className="w-full h-[70vh] md:h-[90vh] bg-gray-900 bg-cover rounded-3xl px-[1rem] sm:px-[5rem] pb-[1rem] flex flex-col justify-center gap-6">
+        <motion.div
+          initial={{ opacity: 0, marginLeft: 25 }}
+          whileInView={{ opacity: 1, marginLeft: 0 }}
+          className="mt-auto sm:mt-0"
+        >
+          <h1 className="text-white text-5xl sm:text-7xl font-bold">
+            Your Expert <br /> Cleaning Services
+          </h1>
+        </motion.div>
         <div className="mt-auto sm:mt-0">
           <button className="sm:w-auto w-full px-4 py-3 bg-teal-400 rounded-xl flex items-center justify-center transition-colors font-bold hover:bg-teal-600">
             <span>Schedule A Cleaning</span>
--- app/components/shared/TopNavigation.tsx
+++ app/components/shared/TopNavigation.tsx
@@ -1,5 +1,6 @@
-import { Calendar, GanttChart, Menu, MenuSquare } from "lucide-react";
+import { Calendar, GanttChart, Menu, MenuSquare, X } from "lucide-react";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 type NavLink = {
   text: string;
@@ -33,14 +34,16 @@ export const TopNavigation = () => {
     "text-gray-500 hover:text-gray-900"
   );
 
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > threshold) {
-        setTopNavContainerClasses("py-3 bg-teal-400 top-4 mx-6 rounded-xl");
+        setTopNavContainerClasses("py-3 bg-teal-400");
         setTopNavButtonClasses("text-gray-50 bg-gray-900 hover:bg-gray-600");
         setTopNavLinkClasses("text-gray-900 hover:text-gray-500");
       } else {
-        setTopNavContainerClasses("py-6 px-6 bg-white top-0 mx-0 rounded-none");
+        setTopNavContainerClasses("py-6 px-6 bg-white mx-0");
         setTopNavButtonClasses("text-gray-900 bg-teal-400 hover:bg-teal-600");
         setTopNavLinkClasses("text-gray-500 hover:text-gray-900");
       }
@@ -53,41 +56,82 @@ export const TopNavigation = () => {
     };
   }, [threshold]);
 
+  useEffect(() => {
+    if (isMobileNavOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isMobileNavOpen]);
+
   return (
-    <nav
-      className={`${topNavContainerClasses} px-6 lg:px-12 transition-all sticky`}
-    >
-      <div className="flex items-center justify-between">
-        <div className="flex-1 flex gap-12 items-center justify-start">
-          <p className="text-2xl tracking-wide font-bold text-gray-900 text-center md:text-left">
-            Skyview Cleaning
-          </p>
+    <>
+      <nav
+        className={`${topNavContainerClasses} px-6 lg:px-12 transition-all top-0 sticky z-50`}
+      >
+        <div className="flex items-center justify-between">
+          <div className="flex-1 flex gap-12 items-center justify-start">
+            <p className="text-2xl tracking-wide font-bold text-gray-900 text-center md:text-left">
+              Skyview Cleaning
+            </p>
 
-          <div className="hidden flex-1 md:flex items-end justify-start gap-6">
-            {navLinks.map(({ text, link }) => (
-              <a
-                key={text}
-                href={link}
-                className={`${topNavLinkClasses} text-lg transition-colors`}
-              >
-                {text}
-              </a>
-            ))}
+            <div className="hidden flex-1 md:flex items-end justify-start gap-6">
+              {navLinks.map(({ text, link }) => (
+                <a
+                  key={text}
+                  href={link}
+                  className={`${topNavLinkClasses} text-lg transition-colors`}
+                >
+                  {text}
+                </a>
+              ))}
+            </div>
           </div>
+
+          <button
+            onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}
+            className="md:hidden p-3 bg-gray-100 rounded-lg text-gray-900"
+          >
+            {isMobileNavOpen ? (
+              <X className="h-5 w-5" />
+            ) : (
+              <GanttChart className="h-5 w-5" />
+            )}
+          </button>
+
+          <button
+            className={`${topNavButtonClasses} hidden px-4 py-2 rounded-md md:flex items-center justify-center transition-colors`}
+          >
+            <Calendar className="hidden md:flex mr-2 h-4 w-4" />
+            <span className="inline-block md:hidden">Schedule</span>
+            <span className="hidden md:inline-block">Schedule A Cleaning</span>
+          </button>
         </div>
 
-        <div className="md:hidden p-3 bg-gray-100 rounded-lg text-gray-900">
-          <GanttChart className="h-5 w-5" />
-        </div>
-
-        <button
-          className={`${topNavButtonClasses} hidden px-4 py-2 rounded-md md:flex items-center justify-center transition-colors`}
+        <motion.div
+          initial={{ y: "-100%" }}
+          animate={{ y: isMobileNavOpen ? "0%" : "-100%" }}
+          transition={{ type: "tween", ease: "easeInOut" }}
+          className="fixed top-0 left-0 w-screen h-screen bg-white -z-10 flex flex-col items-center justify-start pt-20 p-6"
         >
-          <Calendar className="hidden md:flex mr-2 h-4 w-4" />
-          <span className="inline-block md:hidden">Schedule</span>
-          <span className="hidden md:inline-block">Schedule A Cleaning</span>
-        </button>
-      </div>
-    </nav>
+          {navLinks.map(({ text, link }) => (
+            <a
+              key={text}
+              href={link}
+              className={`${topNavLinkClasses} text-lg transition-colors w-full py-6 border-t`}
+              onClick={() => setIsMobileNavOpen(false)}
+            >
+              {text}
+            </a>
+          ))}
+          <button
+            className={`${topNavButtonClasses} px-4 py-2 rounded-md mt-4`}
+            onClick={() => setIsMobileNavOpen(false)}
+          >
+            <span>Schedule A Cleaning</span>
+          </button>
+        </motion.div>
+      </nav>
+    </>
   );
 };
